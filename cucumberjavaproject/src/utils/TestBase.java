b9af3db6bbb95bac2e738c885e440eb9d1a84150
Nandini <nankumar@nextgen.cm>
2019-10-24 22:14:48 -0400

    Initial commit

---
+++ cucumberjavaproject/src/utils/TestBase.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+	public static  WebDriver driver=null;
+	
+	
+	
+	WebElement element =null;
+	
+	public TestBase() {
+		
+		
+	}
+	public static void initDriver() {
+		String path = System.getProperty("user.dir");
+		System.out.print(path);
+		System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		String url  = "https://conversations.dealerinspire.com/";
+		driver.get(url);
+		
+
+				
+	}
+
+	public static void tearDown() {
+		driver.quit();
+	}
+
+	
+	
+}
